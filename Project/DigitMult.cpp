d7c0e9b9c8aa89a43e7f9e2bc70fa3dd5990a7e4
Laura Kissack <lkissack@ugls37.ece.uvic.ca>
2019-07-04 11:17:43 -0700

    Add more details to FSM

--- Project/DigitMult.cpp
+++ Project/DigitMult.cpp
@@ -1,10 +1,11 @@
 #include "systemc.h"
 #include "NN_DigitMult.h"
+#include "lk_moore_fsm"
 
 int sc_main(int argc, char** argv){
 
 	//clock
-
+	
 
 	return 0;
 }
--- Project/lk_moore_fsm.h
+++ Project/lk_moore_fsm.h
@@ -1,20 +1,25 @@
 #pragma once
 
 #include "systemc.h"
+#include <iomanip>
 
 enum ctrl_state {WAIT, EXECUTE, OUTPUT, FINISH}
 
 SC_MODULE(lk_moore_fsm){
-	sc_out <sc_logic> tmux_sel, amux_sel;
+	//sc_out <sc_logic> tmux_sel, amux_sel;
 
 	//signals to tmux
-	sc_in <sc_logic> t_EQ, t_GT, t_LT;
+	//sc_in <sc_logic> t_EQ, t_GT, t_LT;
 
 	//signals to amux
-	sc_in <sc_logic> a_EQ, a_GT, a_LT;
+	//sc_in <sc_logic> a_EQ, a_GT, a_LT;
 
 	//clock
 	sc_in_clk clock;
+
+	//Done and Enable signals for handshaking
+	sc_in <bool> enable;
+	sc_out <bool> done;
 
 	sc_signal<ctrl_state> state, next_state;
 
@@ -24,25 +29,55 @@ SC_MODULE(lk_moore_fsm){
 	SC_CTOR(lk_moore_fsm){
 		SC_CTHREAD(state_reg, clock.pos());
 		SC_METHOD(state_diagram);
-		sensitive<<t_EQ<<t_GT<<t_LT<<a_EQ<<a_GT<<a_LT;
+		sensitive<<enable<<done;
 		//This does not need to be sensitive to all of these just the important ones (tbd)
+		//
 
 	}
 
+	void state_reg(){
+		while(1){
+			//Not sure about this
+			if(enable && !done){
+				state.write(EXECUTE);
+			}
+			else{
+				state.write(next_state.read());			
+			}
+		
+		}
+	}
+
 	void state_diagram(){
-		//deassert mux selects
+		//perform default activities
+		//deassert muxes?
+		next_state.write(state.read());
+		
 
 		switch(state.read()){
 			case WAIT:
-
+				//don't do anything
+				cout<<"Wait state"<<endl;
 				break;
 			case EXECUTE:
+				//perform multiplication
+				cout<<"Execute state"<<endl;
 
+				//assert done signal?
+				done.write(true);
+				//set next state to output?
+				next_state.write(OUTPUT);
 				break;
 
 			case OUPUT:
+			cout<<"Output state" <<endl;
+				//set next state to FINISH
+				next_state.write(FINISH);
 				break;
 			case FINISH:
+				cout<<"Finish state"<<endl;
+				//set next state to WAIT
+				next_state.write(WAIT);
 				break;
 			default:
 				break;
