3eae3e6a5e2084d8129c292694cb7de41c1e47e2
lkissack <33889077+lkissack@users.noreply.github.com>
2019-07-26 08:39:14 -0700

    Remove mux clock

--- Project/lk_mux.h
+++ Project/lk_mux.h
@@ -9,7 +9,7 @@ SC_MODULE(lk_mux){
 	sc_in <sc_logic> sel;
 	sc_in <NN_DIGIT> in;
 	sc_out <NN_DIGIT> out;
-	sc_in_clk clock;
+	//sc_in_clk clock;
 
 	void mux_process(){
 		if(sel.read()==SC_LOGIC_1){
@@ -21,7 +21,8 @@ SC_MODULE(lk_mux){
 	}
 
 	SC_CTOR(lk_mux){
-		SC_CTHREAD(mux_process, clock.pos());
+		SC_METHOD(mux_process);
+		sensitive<<sel;
 	}
 
 };
